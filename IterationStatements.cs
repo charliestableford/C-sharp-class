3e38f6f52637e303936f79202cf0437a7295fc94
Charlie Stableford <charliestableford2@gmail.com>
2020-12-15 16:16:42 -0500

    iterations

---
+++ IterationStatements.cs
@@ -0,0 +1,13 @@
+using System;
+//notes
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
