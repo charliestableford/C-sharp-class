1d035453dd3cb848581a41d70578aa81a8a2090b
Charlie Stableford <charliestableford2@gmail.com>
2020-12-15 16:08:28 -0500

    new file

--- Program.cs
+++ Program.cs
@@ -35,15 +35,18 @@ namespace Conditionals
             //     Console.WriteLine("It's evening");
             // }
             // }
-            bool isGoldCustomer = true;
+            // bool isGoldCustomer = true;
             // float price;
             // if(isGoldCustomer)
             //     price - 19.95f;
             // else
             //     price = 29.95f;
             
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
-            Console.WriteLine(price);
+            // float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            // Console.WriteLine(price);
+
+            // 
+
         }
 
     }
