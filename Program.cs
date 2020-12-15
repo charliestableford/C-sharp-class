8a55a14212622621e2db5748b304b8cb794c729b
Charlie Stableford <charliestableford2@gmail.com>
2020-12-15 15:51:16 -0500

    progress

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+//notes
 
+//starting with
 namespace Course
 {
     class Program
@@ -11,13 +13,29 @@ namespace Course
     }
 }
 
+//conditionals
+//if and else or switch and conditional operators
 namespace Conditionals
 {
     class Program
     {
+        // putting each Console.WriteLine inside its own curly braces is best practice.
         static void Main(string[] args){
-            
+            int hour = 10;
+            if(hour > 0 && hour < 12)
+            {
+                 Console.WriteLine("It's morning");
+            }
+            else if (hour >= 12 && hour <18)
+            {
+                 Console.WriteLine("Its afternoon");
+            }
+            else
+            {
+                Console.WriteLine("It's evening");
+            }
+            }
         }
 
     }
-}
+
