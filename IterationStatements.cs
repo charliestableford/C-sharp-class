ff77c9454b100a91c656342f0b923395ba35ab89
Charlie Stableford <charliestableford2@gmail.com>
2020-12-15 16:27:07 -0500

    notes

--- IterationStatements.cs
+++ IterationStatements.cs
@@ -1,5 +1,6 @@
 using System;
 //notes
+// for, for each - used for anything that has an array in it. While loops - as long as the condition is valid the loop with execute. Usually at the end of while loops we incrememnt. Do while - the loop is executed at least once becuase hte conditon is evalted at hte end of the loop
 
 namespace Iteration
 {
