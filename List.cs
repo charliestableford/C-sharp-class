cc31345c0215896e638dfddbd69542dcef0c184b
Charlie Stableford <charliestableford2@gmail.com>
2020-12-16 14:03:55 -0500

    IndexOf

--- List.cs
+++ List.cs
@@ -1,14 +1,24 @@
 using System;
+using System.Collections.Generic;
 //list type is a generic type. The parameter defines the type of the list. You can make a list of anything.
 // Add(), AddRane(), Remove(), Indexf(), Contains(), Count()
 
-namespace Iteration
+namespace List
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var numbers = new List<int>() {1, 2, 3, 4};
+            numbers.Add(1);
+            numbers.AddRange(new int[3] {5, 6, 7});
+            foreach(var number in numbers)
+            Console.WriteLine(number);
 
+            numbers.IndexOf(1);
+            Console.WriteLine(numbers.IndexOf(1));
+
+            Console.WriteLine(numbers.LastIndexOf(1));
         }
     }
 }
