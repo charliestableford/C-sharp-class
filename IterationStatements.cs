b91cf8e84fd05e3fa6a01d3a1ad9b58abcaef403
Charlie Stableford <charliestableford2@gmail.com>
2020-12-16 11:49:27 -0500

    random string creation

--- IterationStatements.cs
+++ IterationStatements.cs
@@ -83,8 +83,16 @@ namespace Iterations
             // }
 
             var random = new Random();
-            for(var i=0; i<10;i++)
-            Console.WriteLine(random.Next());
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            for(var i=0; i<passwordLength;i++)
+            buffer[i] = (char)('a' + random.Next(0,26));
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+            // loop over between 1-10
+            // Console.WriteLine(random.Next(1,10));
+            // Console.Write((char)random.Next(0,26));
         }
     }
 }
