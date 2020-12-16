f42af84d4e0fe8ba2cad3622175873bc8143be77
Charlie Stableford <charliestableford2@gmail.com>
2020-12-16 15:30:45 -0500

    timers

--- TimeSpan.cs
+++ TimeSpan.cs
@@ -10,6 +10,14 @@ class Program
        var timeSpan1 = new TimeSpan(1,0,0);
        var timeSpan2 = TimeSpan.FromHours(1);
 
+       var start = DateTime.Now;
+       var end = DateTime.Now.AddMinutes(2);
+       var duration = end - start;
+       Console.WriteLine(duration);
+
+        // Properties
+        Console.WriteLine("Minutes" + timeSpan.Minutes);
+        Console.WriteLine("Total Minutes" + timeSpan.TotalMinutes);
     }
 }
 }
