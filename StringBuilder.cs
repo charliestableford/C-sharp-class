f94f26c45cb37148c2897cc4418e8bbca0cc2e27
Charlie Stableford <charliestableford2@gmail.com>
2020-12-17 10:25:50 -0500

    string builder

---
+++ StringBuilder.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+//notes
+
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.Append('-', 10)
+            Console.WriteLine(builder);
+        }
+    }
+}
