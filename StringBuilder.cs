14922d305a7cd03ee1a383abb41b6ebc33049e37
Charlie Stableford <charliestableford2@gmail.com>
2020-12-17 12:37:13 -0500

    OOP vs Procedural

--- StringBuilder.cs
+++ StringBuilder.cs
@@ -1,21 +1,25 @@
-using System;
-using System.Text;
-//notes
+// using System;
+// using System.Text;
+// //notes
 
 
-namespace Iteration
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var builder = new StringBuilder();
-            builder.AppendLine();
-            builder.Append("Header");
-            builder.AppendLine();
-            builder.Append('-', 10);
-            Console.WriteLine(builder);
+// namespace Iteration
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             var builder = new StringBuilder();
+//             builder.AppendLine();
+//             builder.Append("Header");
+//             builder.AppendLine();
+//             builder.Append('-', 10);
+
+//             builder.Replace('-', '+');
+//             builder.Insert(0, new string('-', 10));
+//             Console.WriteLine(builder);
+//             Console.WriteLine(builder[0]);
             
-        }
-    }
-}
+//         }
+//     }
+// }
