2758d1205e2cdb24c051596ed006ed24193ac1be
Charlie Stableford <charliestableford2@gmail.com>
2020-12-17 11:55:09 -0500

    spaces

--- StringBuilder.cs
+++ StringBuilder.cs
@@ -10,8 +10,12 @@ namespace Iteration
         static void Main(string[] args)
         {
             var builder = new StringBuilder();
-            builder.Append('-', 10)
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
             Console.WriteLine(builder);
+            
         }
     }
 }
