fed21d81fc6fc281efb87cc47d401a294f53dc89
Charlie Stableford <charliestableford2@gmail.com>
2020-12-17 13:06:12 -0500

    files and direcoties

--- ProceduralNotes.cs
+++ ProceduralNotes.cs
@@ -1,31 +1,31 @@
-using System;
-// notes: You should always separete the code form the console from the logic code. 
-//Console is only useful in Console applications
+// using System;
+// // notes: You should always separete the code form the console from the logic code. 
+// //Console is only useful in Console applications
 
 
-namespace CSharpFundamentals
-{
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            var name = Console.ReadLine();
-            var reversed = ReverseName(name);
+// namespace CSharpFundamentals
+// {
+//     internal class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             var name = Console.ReadLine();
+//             var reversed = ReverseName(name);
            
-            Console.Write("What's your name?");
-            Console.WriteLine("Reversed name: " + reversed);
+//             Console.Write("What's your name?");
+//             Console.WriteLine("Reversed name: " + reversed);
 
-        }
+//         }
 
-        public static string ReverseName(string name)
-        {
+//         public static string ReverseName(string name)
+//         {
             
-            var array = new char[name.Length];
-            for (var i = name.Length; i>0;i--)
-            array[name.Length - i] = name[i-1];
+//             var array = new char[name.Length];
+//             for (var i = name.Length; i>0;i--)
+//             array[name.Length - i] = name[i-1];
 
-            var reversed = new string(array);
-            return reversed;
-        }
-    }
-}
+//             var reversed = new string(array);
+//             return reversed;
+//         }
+//     }
+// }
