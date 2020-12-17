f5fd3f865b5c2778f4b22d92ad13e707697bf0b5
Charlie Stableford <charliestableford2@gmail.com>
2020-12-17 13:36:27 -0500

    notes

--- Notes.cs
+++ Notes.cs
@@ -2,7 +2,7 @@ using System;
 // notes
 //System.IO - where all the files and directories are stored.
 // you will discover classes based on your needs. 
-// File, FileInfo - both provide methods that relate to files. They have sillmiar interfaces. Creat(), Opy(), Delete(), Exists(), GetAttributes(), Move()
+// File, FileInfo - both provide methods that relate to files. They have sillmiar interfaces. Creat(), Opy(), Delete(), Exists(), GetAttributes(), Move(). These come in pairs. 
 //Directory, DirectoryInfo - similar to file and FileInfo. Static mehtods vs instance methods. CreateDirectory(), Delete(), GetCurrentDirectory(), GetFiles(), Move, GetLogicalDirves()
 // Path - GetDirectoryName(), GetFileName(), GetExtension(), GetTempPAth()
 
