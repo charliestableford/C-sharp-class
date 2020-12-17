64557e562ed689ba342af2b86cd99f6316ca5ed4
Charlie Stableford <charliestableford2@gmail.com>
2020-12-17 13:19:43 -0500

    notes on file, fileInfo

--- Notes.cs
+++ Notes.cs
@@ -1,1 +1,18 @@
+using System;
 // notes
+//System.IO - where all the files and directories are stored.
+// you will discover classes based on your needs. 
+// File, FileInfo - both provide methods that relate to files. They have sillmiar interfaces. Creat(), Opy(), Delete(), Exists(), GetAttributes(), Move()
+//Directory, DirectoryInfo - similar to file and FileInfo. Static mehtods vs instance methods. CreateDirectory(), Delete(), GetCurrentDirectory(), GetFiles(), Move, GetLogicalDirves()
+// Path - GetDirectoryName(), GetFileName(), GetExtension(), GetTempPAth()
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
