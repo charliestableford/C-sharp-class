7ba6e8d2545e8824e35a695537b3ea0b16b6679e
Charlie Stableford <charliestableford2@gmail.com>
2020-12-17 13:02:08 -0500

    return result

--- ProceduralNotes.cs
+++ ProceduralNotes.cs
@@ -1,4 +1,7 @@
 using System;
+// notes: You should always separete the code form the console from the logic code. 
+//Console is only useful in Console applications
+
 
 namespace CSharpFundamentals
 {
@@ -6,16 +9,23 @@ namespace CSharpFundamentals
     {
         public static void Main(string[] args)
         {
-            Console.Write("What's your name?");
             var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+           
+            Console.Write("What's your name?");
+            Console.WriteLine("Reversed name: " + reversed);
 
+        }
+
+        public static string ReverseName(string name)
+        {
+            
             var array = new char[name.Length];
             for (var i = name.Length; i>0;i--)
             array[name.Length - i] = name[i-1];
 
             var reversed = new string(array);
-            Console.WriteLine("Reversed name: " + reversed);
-
+            return reversed;
         }
     }
 }
